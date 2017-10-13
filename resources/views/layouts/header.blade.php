d2fe4efcf3ebb2d73001d940a1a6270e30116f01
Amy Feiling <amyfeiling@u.boisestate.edu>
2017-10-12 20:41:08 -0600

    link references

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,4 @@
 <title>Feiling Books</title>
-<link rel="icon" href="/images/favicon.ico" type="image/icon" />
-<link rel="stylesheet" type="text/css" href="/css/stylesheet.css">
+<link rel="icon" href="./images/favicon.ico" type="image/icon" />
+<link rel="stylesheet" type="text/css" href="./css/stylesheet.css">
 	
